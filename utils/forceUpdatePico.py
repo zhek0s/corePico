e0153475996744079201effd0dc6b3f840685df1
zhek0s <79040843+zhek0s@users.noreply.github.com>
2023-09-20 01:11:43 +0300

    update ftp utils

--- utils/forceUpdatePico.py
+++ utils/forceUpdatePico.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     import time
     from config import configPico
     from core.FTPUpdater.FTPUpdater import FTPUpdater
+    from core.Debug import Debug
     
     spi=SPI(0,2_000_000, mosi=Pin(19),miso=Pin(16),sck=Pin(18))
     nic = network.WIZNET5K(spi,Pin(17),Pin(20))
@@ -14,7 +15,12 @@ if __name__ == "__main__":
     if configPico.ftpUpdate["ftpWork"]:
         time.sleep(3)
         print(nic.ifconfig())
-    ftpUpdater = FTPUpdater(nic)
+    logger=Debug()
+    logger.enablePrintConsole=False
+    logger.logText="forceUpdatePico"
+    logger.warningText="forceUpdatePico"
+    logger.errorText="forceUpdatePico"
+    ftpUpdater = FTPUpdater(nic,logger)
+    ftpUpdater.writeToServer=False
     ftpUpdater.forceUpdatePico=True
-    configPico.ftpUpdate["writeToPico"]=True
     ftpUpdater.Update()
--- utils/ftpUpdaterRunner.py
+++ utils/ftpUpdaterRunner.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     import time
     from config import configPico
     from core.FTPUpdater.FTPUpdater import FTPUpdater
+    from core.Debug import Debug
     
     spi=SPI(0,2_000_000, mosi=Pin(19),miso=Pin(16),sck=Pin(18))
     nic = network.WIZNET5K(spi,Pin(17),Pin(20))
@@ -14,7 +15,13 @@ if __name__ == "__main__":
     if configPico.ftpUpdate["ftpWork"]:
         time.sleep(3)
         print(nic.ifconfig())
-    ftpUpdater = FTPUpdater(nic)
-    configPico.ftpUpdate["writeToPico"]=True
+    logger=Debug()
+    logger.enablePrintConsole=False
+    logger.logText="ftpUpdateRunner"
+    logger.warningText="ftpUpdateRunner"
+    logger.errorText="ftpUpdateRunner"
+    ftpUpdater = FTPUpdater(nic,logger)
+    ftpUpdater.writeToPico=True
+    ftpUpdater.writeToServer=False
     ftpUpdater.Update()
 
